a81cae9bbfbd628f24804acc28225d868af4ced0
Pukar Sharma <pukarsharma112@gmail.com>
2023-06-18 19:57:17 +0545

    fixed cart

--- src/routes/cart/+page.server.js
+++ src/routes/cart/+page.server.js
@@ -18,9 +18,6 @@ export const actions = {
     const currentUser = await event.locals.getCurrentUser();
     const formData = await event.request.formData();
 
-    const data = Object.fromEntries(formData)
-    console.log(data)
-
     const userId = currentUser.id;
     const productId = formData.get("productId");
 
